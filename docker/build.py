c698266c06c2854c6bbdf7444d895c7ac8e759bb
kzha0 <me@ricoduran.net>
2026-01-28 02:24:09 +0800

    refactor build script to auto push images

---
+++ docker/build.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+from dataclasses import dataclass, field
+from datetime import date
+from pathlib import Path
+
+IMG_CONFIGS = [
+    {
+        "label": "soolit-tech/odoo-base",
+        "context": "./base",
+        "dockerfile": "18.Dockerfile",
+    },
+    {
+        "label": "soolit-tech/odoo-oca",
+        "context": "./oca",
+        "dockerfile": "18.Dockerfile",
+    },
+    {
+        "label": "soolit-tech/odoo-builder",
+        "context": "./builder",
+        "dockerfile": "18.Dockerfile",
+    },
+    {
+        "label": "soolit-tech/odoo-builder-oca",
+        "context": "./builder",
+        "dockerfile": "18.Dockerfile",
+        "build_args": {"ODOO_IMAGE": "soolit-tech/odoo-oca"},
+    },
+]
+
+
+@dataclass
+class BuildConfig:
+    label: str
+    context: Path | None = None
+    dockerfile: Path | None = None
+    build_args: dict = field(default_factory=dict)
+    _image_tags: list[str] = field(default_factory=list)
+
+    def build(self, version_tag: str):
+        build_date = date.today().isoformat()
+        major_version = version_tag.split(".")[0]
+
+        tags = [
+            f"{self.label}:latest",
+            f"{self.label}:{version_tag}",
+            f"{self.label}:{major_version}",
+            f"{self.label}:{version_tag}_{build_date}",
+            f"{self.label}:{major_version}_{build_date}",
+        ]
+        args = ["docker", "build"]
+        for tag in tags:
+            args += ["-t", tag]
+
+        for key, value in self.build_args.items():
+            args += ["--build-arg", f"{key}={value}"]
+
+        if self.dockerfile:
+            args += ["-f", self.dockerfile]
+
+        args += ["."]
+
+        subprocess.run(args, cwd=(Path.cwd() / self.context), check=True)
+
+        self._image_tags = tags
+
+    def push(self, registry: str):
+        for tag in self._image_tags:
+            subprocess.run(["docker", "tag", tag, f"{registry}/{tag}"])
+        subprocess.run(["docker", "push", "--all-tags", f"{registry}/{self.label}"])
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-r",
+        "--registry",
+        type=str,
+        help="Target registry. When specified, automatically tags images and pushes images to this registry after build",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        default="18.0",
+        help="Odoo version for tagging images",
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = build_parser()
+    args = parser.parse_args()
+    build_items = [
+        BuildConfig(
+            item.get("label"),
+            item.get("context"),
+            item.get("dockerfile"),
+            item.get("build_args", {}),
+        )
+        for item in IMG_CONFIGS
+    ]
+    for item in build_items:
+        item.build(args.version)
+
+    if args.registry:
+        for item in build_items:
+            item.push(args.registry)
